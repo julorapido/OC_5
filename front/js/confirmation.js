adab926c6e40f4b8f3d665df2b9e5a7f7eeb0a10
= <julesaintho@gmail.com>
2022-08-05 17:53:41 +0200

    Supresstion du panier apres confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,2 +1,7 @@
 const PageUrl = window.location.href.toString();
 document.getElementById("orderId").innerHTML =  PageUrl.substring(PageUrl.length - 36);
+
+/// LA PANIER SE VIDE APRES LA CONFIRMATION
+var EmptyArray = []
+const jsonCartArray = JSON.stringify(EmptyArray);
+sessionStorage.setItem('cartItems', jsonCartArray);
